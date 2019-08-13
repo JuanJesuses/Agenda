977a326aa19ecb4fe16c895d391017b309c5dc32
JuanJesuses <juanj.rodriguez.sanchez@gmail.com>
2019-08-13 22:07:56 +0200

    Se crea la clase Contacto con sus atributos

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -0,0 +1,11 @@
+package org.iesalandalus.programacion.agenda;
+
+public class Contacto {
+	
+	static final String ER_TELEFONO;
+	static final String ER_CORREO;
+	private String nombre;
+	private String telefono;
+	private String correo;
+
+}
